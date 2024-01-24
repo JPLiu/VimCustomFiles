48a0b663506ee3855c7687290ce62365455f568f
JPLiu <luticliu@foxmail.com>
2024-01-24 22:06:06 +0800

    20240124 220606

--- VimCustomFilesClone.bat
+++ VimCustomFilesClone.bat
@@ -5,4 +5,4 @@ git clone git@github.com:JPLiu/VimCustomFiles.git
 pause
 exit
 
-:: Vim-FileSetting vim: set expandtab foldmethod=marker softtabstop=4 shiftwidth=4:
+:: vim: set expandtab foldmethod=marker softtabstop=4 shiftwidth=4:
